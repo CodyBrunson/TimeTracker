55263071bf10405ea55a835a52123bbfee072e3b
CodyBrunson <55360215+CodyBrunson@users.noreply.github.com>
2023-04-11 18:23:11 -0500

    Updated Shared DTOs to Record Struct

--- TimeTracker.Shared/Models/TimeEntry/TimeEntryUpdateRequest.cs
+++ TimeTracker.Shared/Models/TimeEntry/TimeEntryUpdateRequest.cs
@@ -1,8 +1,7 @@
 ﻿namespace TimeTracker.Shared.Models.TimeEntry;
 
-public class TimeEntryUpdateRequest
-{
-    public required String Project { get; set; }
-    public DateTime Start { get; set; } = DateTime.Now;
-    public DateTime? End { get; set; }
-}
+public record struct TimeEntryUpdateRequest(
+    string Project, 
+    DateTime Start,
+    DateTime? End
+);
